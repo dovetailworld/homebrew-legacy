86757ce21606fe685ee5ec230a3e28e050e4e3d7
Pedro Catalão <pedrocatalao@users.noreply.github.com>
2024-10-10 14:10:51 +0100

    Create libxml2.rb

---
+++ libxml2.rb
@@ -0,0 +1,119 @@
+class Libxml2 < Formula
+  desc "GNOME XML library"
+  homepage "http://xmlsoft.org/"
+  url "https://download.gnome.org/sources/libxml2/2.12/libxml2-2.12.8.tar.xz"
+  sha256 "43ad877b018bc63deb2468d71f95219c2fac196876ef36d1bee51d226173ec93"
+  license "MIT"
+
+  # We use a common regex because libxml2 doesn't use GNOME's "even-numbered
+  # minor is stable" version scheme.
+  livecheck do
+    url :stable
+    regex(/libxml2[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "d26b7a67ddf01a47036b5a930d22985ad894c2869335b611bbfd5d33d50d1620"
+    sha256 cellar: :any,                 arm64_ventura:  "33a566efa90fe42f8c06b53b369a10ad140678e52e96a0b0ccaf3939a6695e71"
+    sha256 cellar: :any,                 arm64_monterey: "1e779ad3678b1260f87ac5773c0fbabb0caab2835b51eb8008fbae498e150359"
+    sha256 cellar: :any,                 sonoma:         "b6506871edffe7596f38ebb745c2596d9f9ee56e754e8d562c817199e87bd152"
+    sha256 cellar: :any,                 ventura:        "197af6652b6734b5eea8df35df119f300e42f26cdda9e9558609dc0deeed978a"
+    sha256 cellar: :any,                 monterey:       "20fbab59684333438360b6d5b3ce2717f3a363e7c4110a386fb9e60eb8bd2b99"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88884d551b35cee4b8e07f3478643c875067f33453f6b14915f9879a930ca005"
+  end
+
+  head do
+    url "https://gitlab.gnome.org/GNOME/libxml2.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+  end
+
+  keg_only :provided_by_macos
+
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "pkg-config" => :test
+  depends_on "icu4c"
+  depends_on "readline"
+
+  uses_from_macos "zlib"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", *std_configure_args,
+                          "--sysconfdir=#{etc}",
+                          "--disable-silent-rules",
+                          "--with-history",
+                          "--with-icu",
+                          "--without-python",
+                          "--without-lzma"
+    system "make", "install"
+
+    cd "python" do
+      sdk_include = if OS.mac?
+        sdk = MacOS.sdk_path_if_needed
+        sdk/"usr/include" if sdk
+      else
+        HOMEBREW_PREFIX/"include"
+      end
+
+      includes = [include, sdk_include].compact.map do |inc|
+        "'#{inc}',"
+      end.join(" ")
+
+      # We need to insert our include dir first
+      inreplace "setup.py", "includes_dir = [",
+                            "includes_dir = [#{includes}"
+
+      # Needed for Python 3.12+.
+      # https://github.com/Homebrew/homebrew-core/pull/154551#issuecomment-1820102786
+      with_env(PYTHONPATH: buildpath/"python") do
+        pythons.each do |python|
+          system python, "-m", "pip", "install", *std_pip_args, "."
+        end
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libxml/tree.h>
+
+      int main()
+      {
+        xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
+        xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "root");
+        xmlDocSetRootElement(doc, root_node);
+        xmlFreeDoc(doc);
+        return 0;
+      }
+    EOS
+
+    # Test build with xml2-config
+    args = shell_output("#{bin}/xml2-config --cflags --libs").split
+    system ENV.cc, "test.c", "-o", "test", *args
+    system "./test"
+
+    # Test build with pkg-config
+    ENV.append "PKG_CONFIG_PATH", lib/"pkgconfig"
+    args = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs libxml-2.0").split
+    system ENV.cc, "test.c", "-o", "test", *args
+    system "./test"
+
+    pythons.each do |python|
+      with_env(PYTHONPATH: prefix/Language::Python.site_packages(python)) do
+        system python, "-c", "import libxml2"
+      end
+    end
+  end
+end
